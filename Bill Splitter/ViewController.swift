b220e5ed7aee749280017fd6a12ce18adb2e06aa
krisawyerr <Krisawyerrr@gmail.com>
2024-08-28 15:41:57 -0700

    Initial Commit

---
+++ Bill Splitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bill Splitter
+//
+//  Created by Kris Sawyerr on 8/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
